da217fb1143c97b618ae189665e538703954b068
Fresherpb35 <ds5513063@gmail.com>
2025-08-05 12:17:06 +0530

    Update FindProperties.jsx

--- src/components/FindProperties/FindProperties.jsx
+++ src/components/FindProperties/FindProperties.jsx
@@ -1,46 +1,53 @@
-import React from 'react'
-import './FindProperties.css'
+import React from 'react';
+import './FindProperties.css';
 
 const FindProperties = () => {
   return (
-    <div class="FindProp">
-<h2>Find Properties in These Cities</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div className="FindProp">
+      <h2>Find Properties in These Cities</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
+      <div className="card-wrapper">
+        {/* Top Row */}
+        <div className="card-row">
+          <div
+            className="card card1"
+            style={{ backgroundImage: "url('/14.png')" }}
+          ></div>
 
+          <div
+            className="card card2"
+            style={{ backgroundImage: "url('/15.png')" }}
+          ></div>
 
+          <div
+            className="card card3"
+            style={{ backgroundImage: "url('/16.png')" }}
+          ></div>
+        </div>
 
-<div class="card-wrapper">
-  {/* <!-- Top Row --> */}
-  <div class="card-row">
-    <div class="card card1">
-      
-    </div>
-    <div class="card card2">
-    
-    </div>
-    <div class="card card3">
-      
-    </div>
-  </div>
+        {/* Bottom Row */}
+        <div className="card-row">
+          <div
+            className="card card4"
+            style={{ backgroundImage: "url('/17.png')" }}
+          ></div>
 
-  {/* <!-- Bottom Row --> */}
-  <div class="card-row">
-    <div class="card card4">
-     
-    </div>
-    <div class="card card5">
-      <div class="overlay">
-    
+          <div
+            className="card card5"
+            style={{ backgroundImage: "url('/18.png')" }}
+          >
+            <div className="overlay"></div>
+          </div>
+
+          <div
+            className="card card6"
+            style={{ backgroundImage: "url('/19.png')" }}
+          ></div>
+        </div>
       </div>
     </div>
-    <div class="card card6">
-    </div>
-  </div>
-</div>
+  );
+};
 
-</div>
-  )
-}
-
-export default FindProperties
+export default FindProperties;
